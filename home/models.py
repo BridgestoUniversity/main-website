16372ef008c946b5e3cfebedaac78ffab367530b
Avi Patel <avi.patel0025@gmail.com>
2020-10-01 02:40:11 -0400

    added privacy policy pdfs, added event models, set up event cards, set up recent events section

--- home/models.py
+++ home/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Event(models.Model):
+    # title
+    title = models.CharField(max_length=300, default="Title has not been set")
+    # date
+    date = models.DateField(default="Date has not been set")
+    # countdown description
+    countdownDescription = models.CharField(
+        max_length=200, default="Countdown Description has not been set")
+    # summary
+    summary = models.CharField(max_length=(
+        60*20), default='Summary has not been set')
+    # imageLink
+    imageLink = models.CharField(
+        max_length=200, default="Image link has not been set")
+    # linkType (ie. coming soon, view more, etc.)
+    linkType = models.CharField(max_length=50, default='Coming soon!')
+    # link
+    link = models.CharField(max_length=500, default="Link has not been set")
+
+
+def __str__(self):
+    return self.title
--- home/views.py
+++ home/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, HttpResponse
+from django.db.models import Q
+from .models import Event
 
 
 def homepage(request):
     # return HttpResponse("This is the Bridges Homepage")
-    return render(request, 'home/index.html')
+
+    events = Event.objects
+    eventsLatestOrder = events.order_by('date')
+
+    eventsList = eventsLatestOrder
+
+    return render(request, 'home/recents.html', {"events": eventsList})
