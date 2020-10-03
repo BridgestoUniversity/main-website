4f9b444d5545981be67f2b5bdba23cd47b0e715b
Varun Garg <v8garg@uwaterloo.ca>
2020-10-02 20:46:41 -0400

    static_root fix

--- bridgesWebsite/settings.py
+++ bridgesWebsite/settings.py
@@ -119,20 +119,17 @@ USE_L10N = True
 
 USE_TZ = True
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-
-# Note that BASE_DIR is the directory where manage.py lies
-# Setting STATICFILES_DIRS to access the outmost static folder enables the use of specific static files throughout all apps
-# Will most commonly be used for getting access to our images
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
